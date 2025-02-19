c715e50bef5326f5b3a3d5e1603367df5c924fed
danjun01 <danjun01@users.noreply.github.com>
2025-02-19 03:19:29 -0500

    added route for refreshing access tokens

--- backend/spotify/token_router.py
+++ backend/spotify/token_router.py
@@ -1,7 +1,6 @@
 from ninja import Router, Schema
 from decouple import config
 import requests
-from django.http import HttpRequest
 import base64
 
 SPOTIFY_CLIENT_SECRET = config('SPOTIFY_CLIENT_SECRET')
@@ -11,8 +10,38 @@ REDIRECT_URI = 'http://localhost:5173/auth/callback'
 
 router = Router()
 
+class RefreshToken(Schema):
+    refresh_token: str = ''
+    
 class AuthCode(Schema):
     code: str = ''
+
+@router.post('/refresh')
+async def refresh_access_token(request, data: RefreshToken):
+    if data.refresh_token:
+        # post to spotify/api/token
+        payload = {
+            'grant_type': 'refresh_token',
+            'refresh_token': data.refresh_token,
+        }
+        headers = {
+            'Authorization': 'Basic ' + base64.b64encode(f'{SPOTIFY_CLIENT_ID}:{SPOTIFY_CLIENT_SECRET}'.encode('utf-8')).decode(),
+            'content-type': 'application/x-www-form-urlencoded',
+        }
+        # print(payload)
+        # print(headers)
+        res = requests.post(TOKEN_URL, data=payload, headers=headers)
+        res_data = res.json()
+        # print(res_data)
+        return {
+            'message': 'access token refreshed',
+            'access_token': res_data['access_token'],
+            'refresh_token': res_data['refresh_token']
+        }
+    else:
+        # return error
+        return {'error': 'access token could not be refreshed'}
+
 
 @router.post('/')
 async def get_access_token(request, data: AuthCode):
