e5ad6fb1996f8411f31568fd530848100f2ee05c
danjun01 <danjun01@users.noreply.github.com>
2025-02-22 01:23:04 -0500

    added user_router to Router

--- backend/api.py
+++ backend/api.py
@@ -1,5 +1,8 @@
 from ninja import NinjaAPI
-from backend.spotify.spotify_router import router as spotify_router
+from backend.routers.spotify_router import router as spotify_router
+from backend.routers.user_router import router as user_router
+
 api = NinjaAPI()
 
 api.add_router('/spotify', spotify_router)
+api.add_router('/user', user_router)
